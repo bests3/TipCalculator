cb8c0127878fe5b7f5c8020ebdd487c31911d58d
Sean Best <bests3@farmingdale.edu>
2025-10-11 11:05:54 -0400

    Initial commit of tip calculator

---
+++ src/main/java/org/example/tipcalculator/Launcher.java
@@ -0,0 +1,10 @@
+package org.example.tipcalculator;
+
+import javafx.application.Application;
+
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(TipCalculator.class, args);
+    }
+}
---
+++ src/main/java/org/example/tipcalculator/TipCalculator.java
@@ -0,0 +1,24 @@
+package org.example.tipcalculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class TipCalculator extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("TipCalculator.fxml")));
+        Scene scene = new Scene(root);
+        stage.setTitle("Tip Calculator");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
